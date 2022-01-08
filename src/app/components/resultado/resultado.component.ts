935d1ac3ff454ad6d111f6eaa65cb8b9e23cd46d
Ruben <rubeng.ryu@gmail.com>
2022-01-08 11:55:54 -0300

    primer commit del proyecto.

---
+++ src/app/components/resultado/resultado.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-resultado',
+  templateUrl: './resultado.component.html',
+  styleUrls: ['./resultado.component.css']
+})
+export class ResultadoComponent implements OnInit {
+  bmi: number;
+  resultado: string;
+  interpretacion: string;
+
+  constructor(private route: ActivatedRoute) { 
+    this.resultado = '';
+    this.interpretacion = '';
+
+    this.bmi = +route.snapshot.paramMap.get('valor')!;
+    console.log(this.bmi);
+  }
+
+  ngOnInit(): void {
+    this.getResultado();
+  }
+
+  getResultado(){
+    if(this.bmi > 25){
+      this.resultado = 'Exceso de peso';
+      this.interpretacion = 'Tienes un peso corporal superior al normal. Intente hacer mas ejercicio.';
+    } else if(this.bmi > 18.25){
+      this.resultado = 'Normal';
+      this.interpretacion = 'Tienes un peso corporal normal. ¡Buen Trabajo!';
+    } else{
+      this.resultado = 'Bajo peso';
+      this.interpretacion = 'Tienes un peso corporal más bajo de lo normal. Puedes comer un poco más.';
+    }
+  }
+
+}
